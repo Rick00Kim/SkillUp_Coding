71b19a72c1cf0bd98e4db8383356b02470c813a5
Rick00Kim <dreamx119@gmail.com>
2020-12-26 17:39:16 +0900

    commit algorithm

---
+++ Algorithm/UsingPython/README.md
@@ -0,0 +1,3 @@
+### Algorithm practice using Python
+
+> REFERENCE : <a href ="https://dojang.io/mod/page/view.php?id=2151">Coding Dojang</a>
---
+++ Algorithm/UsingPython/using_python/executer.py
@@ -0,0 +1,9 @@
+from using_python.codility import binary_gap
+
+
+def execute_function():
+    pass
+
+
+if __name__ == "__main__":
+    execute_function()
